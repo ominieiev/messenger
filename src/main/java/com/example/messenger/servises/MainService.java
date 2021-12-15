84b4292e2cc3f91a21ba1066f36399ea42d7a80d
=oleksandr.minieiev <=oleksandr.minieiev@cgm.com>
2021-12-16 00:45:53 +0100

    INIT base

---
+++ src/main/java/com/example/messenger/servises/MainService.java
@@ -0,0 +1,66 @@
+package com.example.messenger.servises;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+@RestController
+public class MainService {
+    private static final String PATH_TO_FILE = "./src/main/resources/static/holder.txt";
+    private static final String MESSAGE_FOR_CHAT_CLEANING = "66f561bb666f561bb6bn837nggfddsgjkkrrtyesfdg2b68372213e00fbf4a2cd6519e0cfcb";
+
+    @GetMapping("/getChat")
+    String getChat() {
+        return readChatFile();
+    }
+
+    @PostMapping("/addToChat")
+    String addToChat(@RequestBody String newMessege) {
+        if (newMessege.equals(MESSAGE_FOR_CHAT_CLEANING)) {
+            cleanChatFile();
+        } else {
+            addToChatFile(newMessege);
+        }
+        return readChatFile();
+    }
+
+    private void addToChatFile(String newMessege) {
+        String chatFile = readChatFile();
+        chatFile = chatFile + "\n" + newMessege;
+        writeToChatFile(chatFile);
+    }
+
+    private String readChatFile() {
+        String content = "";
+        try {
+            content = new String(Files.readAllBytes(Paths.get(PATH_TO_FILE)));
+        } catch (IOException e) {
+            content = "error file reading";
+            e.printStackTrace();
+        }
+        return content;
+    }
+
+    private void writeToChatFile(String text) {
+        try {
+            FileWriter myWriter = new FileWriter(PATH_TO_FILE);
+            myWriter.write(text);
+            myWriter.flush();
+            myWriter.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+
+    private void cleanChatFile() {
+        writeToChatFile("");
+    }
+}
